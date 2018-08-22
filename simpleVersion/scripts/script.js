5cc4e52e4d8e08634c8bd00130244c159de1250c
Spencer Gerdes <skgerdes@yahoo.com>
2018-08-22 05:22:50 -0500

    Basic dynamic ui implementation

--- simpleVersion/scripts/script.js
+++ simpleVersion/scripts/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-	const ALL_ITEMS_LIST = $(".all-items");
-	const FILTERS_LIST = $(".filters");
+	const ALL_ITEMS_CONTAINER = $(".all-items");
+	const FILTERS_CONTAINER = $(".filters");
 	var mockItems = ["Pizza", "Nachos", "Salad", "Pasta", "Steak", "Ice Cream"];
 	var mockFilters = [
 		["Hot", "Cold"],
 		["Appetizer", "Main Course", "Dessert"]
 	];
+
+	var createUIListItem = function(){
+		var li = $(document.createElement("li"));
+		var input = $(document.createElement("input"));
+		var label = $(document.createElement("label"));
+
+		return li.append(input).append(label);
+	};
+
+	var populateUIListFromArray = function(array){
+		$.each(array, function(key, item){
+			ALL_ITEMS_CONTAINER.append(createUIListItem())
+		});
+	};
+
+	populateUIListFromArray(mockItems);
 });
