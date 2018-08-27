6a76a84f541b6c4923cdf48bc932a071610018d8
Spencer Gerdes <skgerdes@yahoo.com>
2018-08-27 11:58:07 -0500

    Restructured input data

--- simpleVersion/scripts/ui.js
+++ simpleVersion/scripts/ui.js
@@ -1,13 +1,51 @@
 const ALL_ITEMS_CONTAINER = $(".all-items");
 const FILTERS_CONTAINER = $(".filters");
-var mockItems = ["Pizza", "Nachos", "Salad", "Pasta", "Steak", "Ice Cream"];
-var mockFilters = [
+
+var mockFilters =
+[
 	["Hot", "Cold"],
 	["Appetizer", "Main Course", "Dessert"]
 ];
 
+var mockItems = 
+[
+	{
+		name: "Pizza",
+		filters: [mockFilters[1][1], mockFilters[0][0]]
+	}, 
+	{
+		name: "Nachos",
+		filters: [mockFilters[1][0], mockFilters[0][0]]
+	}, 
+	{
+		name: "Salad",
+		filters: [mockFilters[1][0], mockFilters[0][1]]
+	},
+	{
+		name: "Pasta",
+		filters: [mockFilters[1][1], mockFilters[0][0]]
+	},
+	{
+		name: "Steak",
+		filters: [mockFilters[1][1], mockFilters[0][0]]
+	},
+	{
+		name: "Ice Cream",
+		filters: [mockFilters[1][2], mockFilters[0][1]]
+	}
+];
+
+
+
+// var mockItems = ["Pizza", "Nachos", "Salad", "Pasta", "Steak", "Ice Cream"];
+// var mockFilters = [
+// 	["Hot", "Cold"],
+// 	["Appetizer", "Main Course", "Dessert"]
+// ];
+
 var createUIListItem = function(item) {
 	var li = $(document.createElement("li"));
+	debugger;
 	var label = createLabelElement(item, {for: item.toLowerCase()});
 	var input = createInputElement({
 		id: item.toLowerCase(),
@@ -40,5 +78,15 @@ var populateUIListFromArray = function(array, container) {
 	});
 };
 
-populateUIListFromArray(mockItems, ALL_ITEMS_CONTAINER);
+
+var items = [];
+
+for (var i = 0; i < mockItems.length; i++) { 
+	items.push(mockItems[i]["name"]) 
+};
+
+populateUIListFromArray(items, ALL_ITEMS_CONTAINER);
 populateUIListFromArray(mockFilters, FILTERS_CONTAINER);
+
+
+
