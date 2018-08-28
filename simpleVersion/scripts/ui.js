262515f28a759aba2115c1557b4c93827c2c0343
Spencer Gerdes <skgerdes@yahoo.com>
2018-08-27 20:37:09 -0500

    Moved execution out of class file

--- simpleVersion/scripts/ui.js
+++ simpleVersion/scripts/ui.js
@@ -1,40 +1,3 @@
-// Init declarations and assignments
-const ALL_ITEMS_CONTAINER = $(".all-items");
-const FILTERS_CONTAINER = $(".filters");
-var wheel;
-var mockFilters =
-[
-	["hot", "cold"],
-	["appetizer", "main course", "dessert"]
-];
-var mockItems = 
-[
-	{
-		name: "Pizza",
-		filters: [mockFilters[1][1], mockFilters[0][0]]
-	}, 
-	{
-		name: "Nachos",
-		filters: [mockFilters[1][0], mockFilters[0][0]]
-	}, 
-	{
-		name: "Salad",
-		filters: [mockFilters[1][0], mockFilters[0][1]]
-	},
-	{
-		name: "Pasta",
-		filters: [mockFilters[1][1], mockFilters[0][0]]
-	},
-	{
-		name: "Steak",
-		filters: [mockFilters[1][1], mockFilters[0][0]]
-	},
-	{
-		name: "Ice Cream",
-		filters: [mockFilters[1][2], mockFilters[0][1]]
-	}
-];
-
 class WheelUI {
 	constructor(items, filters, containers) {
 		this.items = items;
@@ -127,9 +90,3 @@ class WheelUI {
 		wheel = new Wheel(250, this.checkedItems, $("#wheelContainer")[0]);
 	}
 }
-
-// Execution begins here
-var containers = [ALL_ITEMS_CONTAINER, FILTERS_CONTAINER];
-var ui = new WheelUI(mockItems, mockFilters, containers);
-
-
